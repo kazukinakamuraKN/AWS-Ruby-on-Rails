b8c04132d49ceb3b4b89e3821c3445f258e00522
kazukinakamura <kajirikajiri@gmail.com>
2018-10-04 12:40:19 +0000

    Finish layout and routes

--- app/helpers/foo_helper.rb
+++ app/helpers/foo_helper.rb
@@ -1,6 +1,6 @@
 module FooHelper
   
-  def full_title(page_title = '')
+  def foo_full_title(page_title = '')
     base_title = "new application!"
     if page_title.empty?
       base_title
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'static_pages/home'
 
-  get 'static_pages/help'
+  get '/help',    to: 'static_pages#help'
   
   get 'foo/bar'
 
@@ -11,9 +13,11 @@ Rails.application.routes.draw do
   
   root 'static_pages#home'
   
-  get 'static_pages/about'
+  get '/about',   to: 'static_pages#about'
   
   get 'foo/fiz'
   
-  get 'static_pages/contact'
+  get '/contact', to: 'static_pages#contact'
+  
+  get '/signup', to: 'users#new'
 end
