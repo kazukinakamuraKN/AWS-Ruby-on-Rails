32133ad5e52968b54a7fcd6d39e4c34838ee8a12
kazukinakamura <kajirikajiri@gmail.com>
2018-10-04 08:41:29 +0000

    Add a full_title helper

--- app/helpers/foo_helper.rb
+++ app/helpers/foo_helper.rb
@@ -1,2 +1,12 @@
 module FooHelper
+  
+  def full_title(page_title = '')
+    base_title = "new application!"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+  
 end
